1c824bfdca6f50a43317e88c4894ee809cb90838
Lucas Sena <lucasmelosena@gmail.com>
2025-03-06 22:49:28 -0300

    feat: setup project

---
+++ src/main/java/br/dev/lucasena/anota_ai_challenge/AnotaAiChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.dev.lucasena.anota_ai_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaAiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaAiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/lucasena/anota_ai_challenge/AnotaAiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.lucasena.anota_ai_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
