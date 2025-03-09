84a87382f4c54322ce9baefddf6ba696c57f335f
Lucas Sena <lucasmelosena@gmail.com>
2025-03-08 22:52:16 -0300

    feat: add documentation with swagger and unit test

--- src/main/java/br/dev/lucasena/anota_ai_challenge/controllers/CategoryController.java
+++ src/main/java/br/dev/lucasena/anota_ai_challenge/controllers/CategoryController.java
@@ -6,6 +6,8 @@ import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.CreateCategor
 import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.DeleteCategoryUseCase;
 import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.FindAllCategoriesUseCase;
 import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.UpdateCategoryUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
+@Tag(name = "Categorias", description = "Controller para interação com categorias")
 public class CategoryController {
     @Autowired
     private CreateCategoryUseCase createCategoryUseCase;
@@ -25,24 +28,28 @@ public class CategoryController {
     private DeleteCategoryUseCase deleteCategoryUseCase;
 
     @GetMapping
+    @Operation(summary = "Lista todos as categorias")
     public ResponseEntity<List<Category>> findAll() {
         List<Category> categories = this.findAllCategoriesUseCase.execute();
         return ResponseEntity.ok().body(categories);
     }
 
     @PostMapping
+    @Operation(summary = "Cria uma categoria")
     public ResponseEntity<Category> create(@RequestBody CategoryDTO payload) {
         Category category = this.createCategoryUseCase.execute(payload);
         return ResponseEntity.ok().body(category);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza uma categoria")
     public ResponseEntity<Category> update(@PathVariable("id") String id, @RequestBody CategoryDTO payload) {
         Category category = this.updateCategoryUseCase.execute(id, payload);
         return ResponseEntity.ok().body(category);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Exclui uma categoria")
     public ResponseEntity<Category> delete(@PathVariable("id") String id) {
         this.deleteCategoryUseCase.execute(id);
         return ResponseEntity.noContent().build();
--- src/main/java/br/dev/lucasena/anota_ai_challenge/controllers/ProductController.java
+++ src/main/java/br/dev/lucasena/anota_ai_challenge/controllers/ProductController.java
@@ -1,15 +1,10 @@
 package br.dev.lucasena.anota_ai_challenge.controllers;
 
-import br.dev.lucasena.anota_ai_challenge.domain.dtos.CategoryDTO;
 import br.dev.lucasena.anota_ai_challenge.domain.dtos.ProductDTO;
-import br.dev.lucasena.anota_ai_challenge.domain.models.Category;
 import br.dev.lucasena.anota_ai_challenge.domain.models.Product;
-import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.CreateCategoryUseCase;
-import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.DeleteCategoryUseCase;
-import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.FindAllCategoriesUseCase;
-import br.dev.lucasena.anota_ai_challenge.domain.useCases.category.UpdateCategoryUseCase;
 import br.dev.lucasena.anota_ai_challenge.domain.useCases.product.*;
-import jakarta.websocket.server.PathParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
+@Tag(name = "Produtos", description = "Controller para interação com produtos")
 public class ProductController {
     @Autowired
     private CreateProductUseCase createProductUseCase;
@@ -31,30 +27,35 @@ public class ProductController {
     private FindCatalogByOwnerId findCatalogByOwnerId;
 
     @GetMapping
+    @Operation(summary = "Lista todos os produtos")
     public ResponseEntity<List<Product>> findAll() {
         List<Product> products = this.findAllProductsUseCase.execute();
         return ResponseEntity.ok().body(products);
     }
 
     @GetMapping("/catalog/{ownerId}")
+    @Operation(summary = "Retorna o json de um catálogo de produtos dado um owner")
     public ResponseEntity<String> findOwnerCatalog(@PathVariable("ownerId") String ownerId) {
         String catalogUrl = this.findCatalogByOwnerId.execute(ownerId);
         return ResponseEntity.ok().body(catalogUrl);
     }
 
     @PostMapping
+    @Operation(summary = "Cria um produto")
     public ResponseEntity<Product> create(@RequestBody ProductDTO payload) {
         Product product = this.createProductUseCase.execute(payload);
         return ResponseEntity.ok().body(product);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza um produto")
     public ResponseEntity<Product> update(@PathVariable("id") String id, @RequestBody ProductDTO payload) {
         Product product = this.updateProductUseCase.execute(id, payload);
         return ResponseEntity.ok().body(product);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Exclui um produto")
     public ResponseEntity<Product> delete(@PathVariable ("id") String id) {
         this.deleteProductUseCase.execute(id);
         return ResponseEntity.noContent().build();
